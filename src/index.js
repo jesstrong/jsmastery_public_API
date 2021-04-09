43b6928f6b592dfeaf393486fd4b589f499e80e7
JES Strong <janetestrong@yahoo.com>
2021-04-09 12:51:23 -0400

    completed 3 apis

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
-import myMain from "./js/main";
-myMain(); 
+//import myMain from "./js/main";
+//myMain(); 
+
+const url = `http://www.boredapi.com/api/activity/`;
+const button = document.getElementById("button");
+const innerText = document.getElementById("app1");
+button.addEventListener('click', NewActivity);
+function NewActivity(){
+fetch(url).then(response => response.json()).then(data => {
+
+innerText.innerText = data.activity
+}); 
+}    
+
+const url2 = `http://jservice.io/api/random`;
+const button2 = document.getElementById("button2");
+const innerText2 = document.getElementById("app2");
+button2.addEventListener('click', NewTrivia);
+function NewTrivia(){
+    fetch(url2).then(response => response.json()).then(data => {
+        innerText2.innerText = data[0].question;
+    });
+    
+}
+
+const url3 = `https://travelbriefing.org/countries.json`;
+const button3 = document.getElementById("button3");
+const innerText3 = document.getElementById("app3");
+button3.addEventListener('click', TrvlCountries);
+function TrvlCountries(){
+    var rand = Math.floor(Math.random() * 150)
+    fetch(url3).then(response => response.json()).then(data => {
+        innerText3.innerText = data[rand].name; 
+    });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,22 @@
-import Home from "./components/home";
-const appDiv = document.getElementById("app");
+const express = require('express');
+const app = express();
 
-export default () => {
-appDiv.innerText = Home();
 
-}
+
+//import Home from "./components/home";
+//const appDiv = document.getElementById("app");
+
+//export default () => {
+//appDiv.innerText = Home();
+
+//}
+//const url = `http://www.boredapi.com/api/activity/`;
+//const button = document.getElementById("button");
+//const innerText = document.getElementById("app");
+//button.addEventListener('click', NewActivity);
+//function NewActivity(){
+//fetch(url).then(response => response.json()).then(data => {
+
+//innerText.innerText = data.activity
+//}); 
+//}    
